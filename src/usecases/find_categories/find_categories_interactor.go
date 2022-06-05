45f07a981ea414ca536d041719ac0d32f9d9c9ba
axel-andrade <ajaxeljunio@gmail.com>
2022-06-05 15:47:15 -0300

    chore: mock response to test perfomance

--- src/usecases/find_categories/find_categories_interactor.go
+++ src/usecases/find_categories/find_categories_interactor.go
@@ -1,5 +1,7 @@
 package find_categories
 
+import "go_gift_list_api/src/entities"
+
 type FindCategoriesInteractor struct {
 	Gateway FindCategoriesGateway
 }
@@ -9,10 +11,21 @@ func BuildFindCategoriesInteractor(g FindCategoriesGateway) *FindCategoriesInter
 }
 
 func (bs *FindCategoriesInteractor) Execute(input FindUserInputDTO) (*FindCategoriesOutputDTO, error) {
-	data, err := bs.Gateway.FindCategoriesPaginate(&input)
-	if err != nil {
-		return nil, err
-	}
+	// data, err := bs.Gateway.FindCategoriesPaginate(&input)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	return data, nil
+	// return data, nil
+	return &entities.PaginateResult{
+		Docs:        nil,
+		TotalDocs:   0,
+		TotalPages:  0,
+		Limit:       0,
+		Page:        0,
+		HasPrevPage: true,
+		HasNextPage: true,
+		PrevPage:    0,
+		NextPage:    0,
+	}, nil
 }
