7f3d04192fc36308c024fd0cdb79dcf76845f3a5
axel-andrade <ajaxeljunio@gmail.com>
2022-06-23 20:49:50 -0300

    refactor: change image_filename to image

--- src/adapters/presenters/common/gift_presenter.go
+++ src/adapters/presenters/common/gift_presenter.go
@@ -6,15 +6,15 @@ import (
 )
 
 type GiftFormatted struct {
-	ID            entities.UniqueEntityID `json:"id" bson:"id"`
-	Name          string                  `json:"name" bson:"name"`
-	Category      CategoryFormatted       `json:"category" bson:"category"`
-	Available     bool                    `json:"available" bson:"available"`
-	ImageFilename string                  `json:"image_filename" bson:"image_filename"`
-	Quantity      int64                   `json:"quantity" bson:"quantity"`
-	PriceGrade    int64                   `json:"price_grade" bson:"price_grade"`
-	CreatedAt     time.Time               `json:"created_at" bson:"created_at"`
-	UpdatedAt     time.Time               `json:"updated_at" bson:"updated_at"`
+	ID         entities.UniqueEntityID `json:"id" bson:"id"`
+	Name       string                  `json:"name" bson:"name"`
+	Category   CategoryFormatted       `json:"category" bson:"category"`
+	Available  bool                    `json:"available" bson:"available"`
+	Image      string                  `json:"image" bson:"image"`
+	Quantity   int64                   `json:"quantity" bson:"quantity"`
+	PriceGrade int64                   `json:"price_grade" bson:"price_grade"`
+	CreatedAt  time.Time               `json:"created_at" bson:"created_at"`
+	UpdatedAt  time.Time               `json:"updated_at" bson:"updated_at"`
 }
 
 type GiftPresenter struct {
@@ -23,15 +23,15 @@ type GiftPresenter struct {
 
 func (ptr *GiftPresenter) Format(gift entities.Gift) GiftFormatted {
 	return GiftFormatted{
-		ID:            gift.ID,
-		Name:          gift.Name,
-		Category:      ptr.categoryPtr.Format(gift.Category),
-		Available:     gift.Available,
-		ImageFilename: gift.ImageFilename,
-		Quantity:      gift.Quantity,
-		PriceGrade:    gift.PriceGrade,
-		CreatedAt:     gift.CreatedAt,
-		UpdatedAt:     gift.UpdatedAt,
+		ID:         gift.ID,
+		Name:       gift.Name,
+		Category:   ptr.categoryPtr.Format(gift.Category),
+		Available:  gift.Available,
+		Image:      gift.Image,
+		Quantity:   gift.Quantity,
+		PriceGrade: gift.PriceGrade,
+		CreatedAt:  gift.CreatedAt,
+		UpdatedAt:  gift.UpdatedAt,
 	}
 }
 
