02d3646dc02fac18a146d168e2b9e9d4418d2f5d
axel-andrade <ajaxeljunio@gmail.com>
2022-06-24 00:42:39 -0300

    force deploy

--- src/infra/http/server/server.go
+++ src/infra/http/server/server.go
@@ -16,13 +16,6 @@ type Server struct {
 
 func NewServer() Server {
 	r := gin.Default()
-	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowOrigins = []string{"http://localhost:3000"}
-	corsConfig.AllowCredentials = true
-	corsConfig.AddAllowHeaders("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers", "Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization")
-	corsConfig.AddAllowMethods("GET", "POST", "PUT", "DELETE")
-
-	r.Use(cors.New(corsConfig))
 
 	return Server{
 		port:   os.Getenv("PORT"),
@@ -32,7 +25,14 @@ func NewServer() Server {
 
 func (s *Server) Run() {
 	router := routes.ConfigRoutes(s.server)
-	router.SetTrustedProxies(nil)
+	// router.SetTrustedProxies(nil)
 
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:3000"}
+	corsConfig.AllowCredentials = true
+	corsConfig.AddAllowHeaders("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers", "Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization")
+	corsConfig.AddAllowMethods("GET", "POST", "PUT", "DELETE")
+
+	router.Use(cors.New(corsConfig))
 	log.Fatal(router.Run(":" + s.port))
 }
