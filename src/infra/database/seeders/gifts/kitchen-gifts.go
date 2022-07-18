e2504a7609d40b3fcb1fbe2e1355d85fd77f2ba9
axel-andrade <ajaxeljunio@gmail.com>
2022-07-18 20:54:24 -0300

    chore: add ajustes

--- src/infra/database/seeders/gifts/kitchen-gifts.go
+++ src/infra/database/seeders/gifts/kitchen-gifts.go
@@ -53,7 +53,7 @@ func (g *GiftSeeder) createKitchenGifts(db *gorm.DB) {
 		{
 			Name:       "Depurador",
 			CategoryID: kitchenCategoryID,
-			Available:  2,
+			Available:  0,
 			Image:      "depurador",
 			Quantity:   1,
 			PriceGrade: 3,
@@ -205,7 +205,7 @@ func (g *GiftSeeder) createKitchenGifts(db *gorm.DB) {
 		{
 			Name:       "Rodinho de pia",
 			CategoryID: kitchenCategoryID,
-			Available:  2,
+			Available:  0,
 			Image:      "rodinho_pia",
 			Quantity:   1,
 			PriceGrade: 1,
--- src/infra/database/seeders/gifts/living-room-gifts.go
+++ src/infra/database/seeders/gifts/living-room-gifts.go
@@ -13,7 +13,7 @@ func (g *GiftSeeder) createlivingRoomGifts(db *gorm.DB) {
 		{
 			Name:       "Sofá cama",
 			CategoryID: livingRoomCategoryID,
-			Available:  2,
+			Available:  0,
 			Image:      "sofa_cama",
 			Quantity:   1,
 			PriceGrade: 3,
